c4984336479628517fcb28bbcdcb3608ad9b5936
Bronson Quick <bronson@bronsonquick.com.au>
2019-11-15 15:17:36 +1000

    Add ini file

--- modules/wonolog/manifests/init.pp
+++ modules/wonolog/manifests/init.pp
@@ -1,7 +1,8 @@
 # A class to setup and install Wonolog
 class wonolog (
 	$config,
-	$content  = $config[mapped_paths][content]
+	$content = $config[mapped_paths][content],
+	$version = $config[php]
 ) {
 	file { "${content}/mu-plugins":
 		ensure => 'directory',
@@ -18,12 +19,36 @@ class wonolog (
 		path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
 		cwd         => "${content}/mu-plugins/wonolog",
 		command     => 'composer require inpsyde/wonolog',
-		require     => [ File["${content}/mu-plugins/wonolog"], Exec["install composer"] ],
+		require     => [
+			File["${content}/mu-plugins/wonolog"],
+			File["/etc/php/${version}/cli/conf.d/wonolog.ini"],
+			File["/etc/php/${version}/fpm/conf.d/wonolog.ini"],
+			Exec["install composer"]
+		],
 		environment => ['HOME=/home/vagrant'],
 	}
 
 	file { "${content}/mu-plugins/wonolog.php":
 		ensure  => file,
 		content => template('wonolog/wonolog.php.erb'),
+		require => Exec['install wonolog']
+	}
+
+	file { "/etc/php/${version}/cli/conf.d/wonolog.ini":
+		ensure   => file,
+		content  => template('wonolog/wonolog.ini.erb'),
+		owner    => 'root',
+		group    => 'root',
+		mode     => '0644',
+		notify   => Service["php${version}-fpm"]
+	}
+
+	file { "/etc/php/${version}/fpm/conf.d/wonolog.ini":
+		ensure   => file,
+		content  => template('wonolog/wonolog.ini.erb'),
+		owner    => 'root',
+		group    => 'root',
+		mode     => '0644',
+		notify   => Service["php${version}-fpm"]
 	}
 }
